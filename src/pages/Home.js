c8767994c981fadd32d5a5f297858f591c9ddb7a
andre-vg <avictorg@gmail.com>
2022-05-12 21:26:48 -0300

    darkmode

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,20 +11,20 @@ function Home() {
 
   return (
     <div className={isActive ? "white" : "dark"}>
-      <div className="bg-neutral-300 absolute h-full w-full dark:bg-gray-700 transition-colors duration-1000">
+      <div className="bg-neutral-300 absolute h-full w-full dark:bg-neutral-700 transition-colors duration-1000">
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
           <div className="my-4">
-            <h1 className="font-bold bebas text-gray-500 dark:text-slate-200 text-9xl transition-colors duration-1000">
+            <h1 className="font-bold bebas text-neutral-700 dark:text-slate-200 text-9xl transition-colors duration-1000">
               FUT<strong className="text-green-600">QUIZ</strong>
             </h1>
-            <h2 className="font montserrar text-gray-500 dark:text-slate-200 text-2xl transition-colors duration-1000">
+            <h2 className="font montserrar text-neutral-700 dark:text-slate-200 text-2xl transition-colors duration-1000">
               Responda as perguntas a seguir para testar seu conhecimento sobre
               futebol!
             </h2>
             <br />
           </div>
           <Link to="/dev">
-            <button className="bg-green-600 hover:scale-125 text-gray-500 dark:text-slate-200 font-bold py-2 px-4 rounded mt-4 text-center shadow-xl  dark:shadow-lg dark:shadow-green-600 duration-500">
+            <button className="bg-green-600 hover:scale-125 text-neutral-700 dark:text-slate-200 font-bold py-2 px-4 rounded mt-4 text-center shadow-xl  dark:shadow-lg dark:shadow-green-600 duration-500">
               Jogar
             </button>
           </Link>
