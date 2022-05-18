6c83202a1e6de382f94acbc6c8d255f807a246a8
andre-vg <avictorg@gmail.com>
2022-05-18 20:10:34 -0300

    v1

--- src/pages/Framer.js
+++ src/pages/Framer.js
@@ -8,7 +8,12 @@ import { CgDarkMode } from "react-icons/cg";
 import { FiTwitter } from "react-icons/fi";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { MdOutlineContentCopy } from "react-icons/md";
-import { useMotionValue, useTransform, motion } from "framer-motion";
+import {
+  useMotionValue,
+  useTransform,
+  motion,
+  useAnimation,
+} from "framer-motion";
 import { IoMdTrophy } from "react-icons/io";
 
 function Framer() {
@@ -28,6 +33,7 @@ function Framer() {
       geraQuestao();
     } else {
       console.log("Você errou!");
+      animando();
       setVidas(vidas - 1);
     }
   };
@@ -39,6 +45,7 @@ function Framer() {
       geraQuestao();
     } else {
       console.log("Você errou!");
+      animando();
       setVidas(vidas - 1);
     }
   };
@@ -57,20 +64,20 @@ function Framer() {
         <div className="container absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
           <div className="row">
             <div className="col-12">
-              <h1 className="font-bold text-green-600 bebas text-9xl ">
+              <h1 className="font-bold text-green-600 bebas text-8xl sm:text-[11rem] ">
                 GAME
                 <strong className="dark:text-neutral-300 text-neutral-700">
                   OVER
                 </strong>
               </h1>
               <br />
-              <h2 className="text-neutral-700 font-bold text-3xl dark:text-neutral-300">
+              <h2 className="text-neutral-700 font-bold text-2xl sm:text-3xl dark:text-neutral-300">
                 Sua pontuação foi:
                 <strong className="font-extrabold text-green-600 text-4xl ml-1">
                   {ponto}
                 </strong>
               </h2>
-              <h2 className="text-neutral-700 font-bold text-3xl dark:text-neutral-300">
+              <h2 className="text-neutral-700 font-bold text-2xl sm:text-3xl dark:text-neutral-300">
                 Maior pontuação:
                 <strong className="font-extrabold text-green-600 text-4xl ml-1">
                   {localStorage.getItem("MaiorScore")}
@@ -81,7 +88,7 @@ function Framer() {
           <div className="row">
             <a href="/dev">
               <button className="bg-green-600 scale-100 hover:scale-110 transition-all duration-300 p-4 rounded-3xl mt-8 shadow-lg hover:shadow-xl">
-                <h2 className="text-neutral-700 text-3xl font-extrabold dark:text-neutral-300">
+                <h2 className="text-neutral-700 text-2xl sm:text-3xl font-extrabold dark:text-neutral-300">
                   Jogar novamente
                 </h2>
               </button>
@@ -108,7 +115,7 @@ function Framer() {
                   id="twitter"
                   className="scale-0 font-bold group-hover:scale-100 transition-all duration-300 bg-green-600 mt-6 text-neutral-700 py-2 px-2 rounded-xl shadow-md justify-center"
                 >
-                  Compartilhar
+                  Twitter
                 </div>
               </TwitterShareButton>
             </div>
@@ -181,10 +188,13 @@ function Framer() {
     if (vidas === 0) {
       handleGameOver();
     } else {
-      console.log(xInput);
       geraQuestao();
     }
   }, [vidas]);
+
+  useEffect(() => {
+    gameStart();
+  }, []);
 
   const x = useMotionValue(0);
   const xInput = [
@@ -198,6 +208,29 @@ function Framer() {
     "--tw-shadow-color: #16a34a",
   ]);
 
+  const animar = useAnimation();
+
+  const gameStart = async () => {
+    animar.start({
+      x: [0, -150, 0, 150, 0],
+      transition: {
+        duration: 2.5,
+        ease: "easeInOut",
+      },
+    });
+  };
+
+  const animando = async () => {
+    animar.start({
+      //make a shake animation
+      x: [0, -20, 0, 20, 0, -15, 0, 15, 0, -10, 0, 10, 0, -5, 0, 5, 0],
+      transition: {
+        duration: 0.6,
+        ease: "easeInOut",
+      },
+    });
+  };
+
   return (
     <>
       <div className={isActive ? "dark" : "white"}>
@@ -209,9 +242,9 @@ function Framer() {
             className="mt-[12%] md:mt-[8%] lg:mt-[4%] xl:mt-[3%] inline-flex w-full justify-center"
             id="pontos"
           >
-            <IoMdTrophy className="text-green-600" size={80} />
+            <IoMdTrophy className="text-green-600" size={85} />
             <h2>
-              <strong className="bebasGrudado ml-4 text-7xl font-bold">
+              <strong className="bebasGrudado ml-4 text-8xl font-bold text-neutral-900">
                 {ponto}
               </strong>
             </h2>
@@ -220,20 +253,20 @@ function Framer() {
             <div class="center">
               <motion.div
                 style={{ cor }}
-                animate={{
-                  x: [0, -150, 0, 150, 0],
-                  transition: {
-                    duration: 2.5,
-                    ease: "easeInOut",
-                  },
-                }}
+                animate={
+                  // x: [0, -150, 0, 150, 0],
+                  // transition: {
+                  //   duration: 2.5,
+                  //   ease: "easeInOut",
+                  // },
+                  animar
+                }
                 class="property-card"
                 drag
                 dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                onDrag={(e, info) => {
-                  x.set(info.point.x);
-                  console.log(info.point.x);
-                }}
+                // onDrag={(e, info) => {
+                //   x.set(info.point.x);
+                // }}
                 onDragEnd={(e, info) => {
                   if (info.point.x >= (window.innerWidth * 3) / 4) {
                     handleVerdadeiro();
