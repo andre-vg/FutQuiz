43bf60c4120b09a8a671d46bb0b740f6a87527b1
andre-vg <avictorg@gmail.com>
2022-05-29 17:15:58 -0300

    logo compartilhamento

--- src/pages/GameSimple.js
+++ src/pages/GameSimple.js
@@ -32,7 +32,7 @@ function GameSimple() {
   const game = useRef(null);
   const [questaoUsada, setQuestaoUsada] = useState([]);
   const [play] = useSound(FimGame, { volume: 0.3 });
-  const [somTrave] = useSound(SomTrave, { volume: 1 });
+  const [somTrave] = useSound(SomTrave, { volume: 5 });
 
   const handleFalso = () => {
     if (perguntas[aleatorio].resposta === "F") {
